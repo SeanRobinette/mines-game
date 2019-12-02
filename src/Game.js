3480870304e773be5837a5e2e2743186dc5dfbd7
SeanRobinette <smrobinette@gmail.com>
2019-12-02 11:46:34 -0500

    Add support for changing game settings

--- src/Game.js
+++ src/Game.js
@@ -6,7 +6,10 @@ export class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            board: new Board(props.width, props.height, props.mines)
+            board: new Board(10, 10, 15),
+            newWidth: 10,
+            newHeight: 10,
+            newMines: 15
         };
     }
     render() {
@@ -23,16 +26,45 @@ export class Game extends React.Component {
         } else {
             statusMessage = <div>Click on tiles to reveal them.</div>
         }
+        const newWidth = this.state.newWidth;
+        const newHeight = this.state.newHeight;
+        const newMines = this.state.newMines;
+
         return (
             <div>
                 {grid}
                 {statusMessage}
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><label>Width:</label></td>
+                            <td><input type="number" value={newWidth} name="newWidth" onChange={this.handleFormChange}/></td>
+                        </tr>
+                        <tr>
+                            <td><label>Height:</label></td>
+                            <td><input type="number" value={newHeight} name="newHeight" onChange={this.handleFormChange}/></td>
+                        </tr>
+                        <tr>
+                            <td><label>Mines:</label></td>
+                            <td><input type="number" value={newMines} name="newMines" onChange={this.handleFormChange}/></td>
+                        </tr>
+                    </tbody>
+                </table>
                 <div className="row"><button onClick={() => this.reset()}>New Game</button></div>
             </div>
         )
     }
     reset() {
-        this.setState({board: new Board(this.props.width, this.props.height, this.props.mines)})
+        this.setState({board: new Board(this.state.newWidth, this.state.newHeight, this.state.newMines)})
+    }
+    handleFormChange = (e) => {
+        let value = e.target.value;
+        if(typeof value === 'string') {
+            value = parseInt(value);
+        }
+        this.setState({
+            [e.target.name]: value
+        });
     }
     handleDoubleClick(r, c) {
         let board = this.state.board;
